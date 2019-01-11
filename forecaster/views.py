d2ed289416fcfc8e5081a9786a2722013869fbac
Jake Weir <jweir136@yahoo.com>
2019-01-10 17:58:51 -0800

    t

--- forecaster/views.py
+++ forecaster/views.py
@@ -3,80 +3,26 @@ from django.http import HttpResponse
 
 from .models import Updates
 from datetime import datetime
+import os
 
 import pickle
 import requests
 from bs4 import BeautifulSoup
 import html5lib
+import quandl
 # Create your views here.
-
-def get():
-	con = 0
-	
-	while con == 0:
-		try:
-			url = "https://www.treasury.gov/resource-center/data-chart-center/interest-rates/pages/TextView.aspx?data=yieldAll"
-			data = requests.get(url).text
-			soup = BeautifulSoup(data, 'html5lib')
-			
-			main_table = soup.find_all("table")[1]
-			
-			today_yield = main_table.find_all("tr")[len(main_table.find_all("tr")) - 1]
-			month_ago_yield = main_table.find_all("tr")[len(main_table.find_all("tr")) - 21]
-		
-			con = 1
-			
-			return float(today_yield.find_all("td")[5].text) - float(month_ago_yield.find_all("td")[5].text)
-		except:
-			pass		
-			
-def vix():
-	con = 0
-	
-	while con == 0:
-		try:
-			url = "https://finance.yahoo.com/quote/%5EVIX/history?period1=1538895600&period2=1546848000&interval=1d&filter=history&frequency=1d"
-			data = requests.get(url).text
-			soup = BeautifulSoup(data, 'html5lib')
-			
-			main_table = soup.find_all("table")[0]
-			
-			main_tbody = main_table.find("tbody")
-			
-			vix_close1 = float(main_tbody.find_all("tr")[0].find_all("td")[5].text)
-			vix_close2 = float(main_tbody.find_all("tr")[21].find_all("td")[5].text)
-			
-			con = 1
-			
-			return vix_close1 - vix_close2
-		except:
-			pass
 
 def index(requests):
 	# if the system was not updated today...
 	obj = Updates.objects.all()
 	last_day = int(obj.values('last_update')[0]['last_update'])
-	"""
-	if last_day != int(datetime.now().day):
-		print(True)
-		# run the scaping functions
-		delta_yield, delta_vix = get(), vix()
-		# add values into model
-		obj.delete()
-		print(delta_yield, delta_vix)
-		new_obj = Updates.objects.create(last_update=str(datetime.now().day), delta_yield=str(delta_yield), delta_vix=str(delta_vix))
-		new_obj.save()
-	else:
-		delta_yield = float(obj.values('delta_yield')[0]['delta_yield'])
-		delta_vix = float(obj.values('delta_vix')[0]['delta_vix'])
-		print(delta_yield, delta_vix)
-	"""
-	from rq import Queue
-	from .worker import conn
-	q = Queue(connection=conn)
-	from .utils import test
-	result = q.enqueue(test)
-	#model = pickle.load(open(r"C:\Users\jweir\Data Science Practice 1\datasets\basic_regression\AAPL1\recession_forecast\recession_forecast\forecaster/recession_model.sav", "rb"))
-	#status = model.predict([[delta_yield, delta_vix]])
-		
-	return render(requests, "forecaster/index.html", {"status":status})
+	vix = quandl.get("CHRIS/CBOE_VX1", paginate=True)
+	vix_close = vix.values[len(vix) - 1][4] - vix.values[len(vix) - 3][4]
+	yield_curve = quandl.get("FRED/IOER", paginate=True)
+	yield_data = yield_curve.values[len(yield_curve) - 1][0] - yield_curve.values[len(yield_curve) - 32][0]
+	dir = os.getcwd()
+	filename = r"{}\forecaster\model.sav".format(dir)
+	file = open(filename, "rb")
+	model = pickle.load(file)
+	status = model.predict([[yield_data, vix_close]])
+	return render(requests, "forecaster/index.html", {'status':status})
